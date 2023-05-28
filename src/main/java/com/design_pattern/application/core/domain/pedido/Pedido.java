ef8fa4752edabfe86eb61f1e2be12e7c445db08f
ANDERSON PEREIRA <andy2903.alp@gmail.com>
2023-05-28 19:47:53 -0300

    feat: design pattern with tests

--- src/main/java/com/design_pattern/application/core/domain/pedido/Pedido.java
+++ src/main/java/com/design_pattern/application/core/domain/pedido/Pedido.java
@@ -1,5 +1,21 @@
 package com.design_pattern.application.core.domain.pedido;
 
-public abstract class Pedido {
 
+import com.design_pattern.application.core.domain.frete.Frete;
+
+public abstract class Pedido {
+    private double valorPedido;
+    private Frete frete;
+
+    public void setValorPedido(double valorPedido) {
+        this.valorPedido = valorPedido;
+    }
+
+    public void setFrete(Frete frete) {
+        this.frete = frete;
+    }
+
+    public double calcularFrete() {
+        return frete.calcula(valorPedido);
+    }
 }
